f63f02e660c92297892adc3fb5215b1385d7e6a7
Amaury <loubry.amaury@gmail.com>
2024-11-22 17:31:49 +0100

    progress

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: aloubry <aloubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 13:58:06 by aloubry           #+#    #+#             */
-/*   Updated: 2024/11/22 14:35:13 by aloubry          ###   ########.fr       */
+/*   Updated: 2024/11/22 17:31:11 by aloubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,23 @@
 
 typedef struct s_data
 {
-	int		number_of_philosophers;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	int		number_of_times_each_philosopher_must_eat;
-	int 	time_start;
+	int			number_of_philosophers;
+	int			time_to_die;
+	int			time_to_eat;
+	int			time_to_sleep;
+	int			number_of_times_each_philosopher_must_eat;
+	long long	time_start;
 } t_data;
+
+typedef struct s_philo
+{
+	int		id;
+	char 	*right_fork;
+	char 	*left_fork;
+	t_data 	*data;
+} t_philo;
+
+
 
 int	ft_atoi(const char *nptr)
 {
@@ -45,7 +55,7 @@ int	ft_atoi(const char *nptr)
 	return (num * mult);
 }
 
-int get_time()
+long long get_time()
 {
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
@@ -68,29 +78,59 @@ t_data parse_argv(int argc, char **argv)
 	return (data);
 }
 
-int get_timestamp(t_data data)
+long long get_timestamp(long long time_start)
 {
-	return (get_time() - data.time_start);
+	return (get_time() - time_start);
+}
+
+//0 = taken, 1 = free
+void take_fork()
+{
+
 }
 
  // eat > sleep > think > eat
 
-void *philo_sleep(void *void_data)
+void *philo_think(void *void_data)
 {
-	t_data data = *(t_data *)void_data;
-	printf("%d %d is sleeping\n", get_timestamp(data), data.number_of_philosophers);
-	usleep(data.time_to_sleep * 1000);
-	// philo_think(data);
+	t_philo philo = *(t_philo *)void_data;
+	printf("%05lld %d is thinking\n", get_timestamp(philo.data->time_start), philo.id);
 	return NULL;
 }
 
-void *philo_think(void *void_data)
+void *philo_sleep(void *void_data)
 {
-	t_data data = *(t_data *)void_data;
-	printf("%d %d is thinking\n", get_timestamp(data), data.number_of_philosophers);
-	philo_sleep(void_data);
-	// philo_eat(data);
+	t_philo philo = *(t_philo *)void_data;
+	printf("%05lld %d is sleeping\n", get_timestamp(philo.data->time_start), philo.id);
+	usleep(philo.data->time_to_sleep * 1000);
+	philo_think(void_data);
 	return NULL;
+}
+
+
+t_philo *init_philosophers(t_data *data, char *forks)
+{
+	int i;
+	t_philo *philosophers;
+
+	philosophers = malloc(sizeof(t_philo) * data->number_of_philosophers);
+	if(!philosophers)
+		return (NULL);
+	i = 0;
+	while(i < data->number_of_philosophers)
+	{
+		philosophers[i].id = i + 1;
+		philosophers[i].left_fork = &forks[i];
+		if(i == data->number_of_philosophers - 1 && data->number_of_philosophers > 1)
+			philosophers[i].right_fork = &forks[0];
+		else if(i < data->number_of_philosophers - 1)
+			philosophers[i].right_fork = &forks[i + 1];
+		else
+			philosophers[i].right_fork = NULL;
+		philosophers[i].data = data;
+		i++;
+	}
+	return (philosophers);
 }
 
 int main(int argc, char **argv)
@@ -100,15 +140,36 @@ int main(int argc, char **argv)
 		printf("Usage: %s number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]\n", argv[0]);
 		return (1);
 	}
+
+	//setup
 	t_data data = parse_argv(argc, argv);
-	pthread_t *philosophers = malloc(sizeof(pthread_t) * data.number_of_philosophers);
-	for (int i = 0; i < data.number_of_philosophers; i++)
+	char *forks = malloc(sizeof(char) * data.number_of_philosophers);
+	if(!forks)
+		return (1);
+	memset(forks, 1, data.number_of_philosophers);
+	t_philo *philosophers = init_philosophers(&data, forks);
+	if(!philosophers)
 	{
-		pthread_create(&philosophers[i], NULL, philo_think, &data);
+		free(forks);
+		return (1);
 	}
-	for (int i = 0; i < data.number_of_philosophers; i++)
+	
+	//start
+	pthread_t *philo_threads = malloc(sizeof(pthread_t) * data.number_of_philosophers);
+	int i = 0;
+	while(i < data.number_of_philosophers)
 	{
-		pthread_join(philosophers[i], NULL);
+		pthread_create(&philo_threads[i], NULL, philo_sleep, philosophers + i);
+		i++;
 	}
+	i = 0;
+	while(i < data.number_of_philosophers)
+	{
+		pthread_join(philo_threads[i], NULL);
+		i++;
+	}
+	free(philo_threads);
 	free(philosophers);
+	free(forks);
+	return (0);
 }
