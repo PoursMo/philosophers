25daa2088118cf80697e51e89f460d20e665797c
Amaury <loubry.amaury@gmail.com>
2024-11-29 13:22:15 +0100

    adapting code for bonus

--- philo_bonus/actions.c
+++ philo_bonus/actions.c
@@ -6,7 +6,7 @@
 /*   By: aloubry <aloubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 17:42:28 by aloubry           #+#    #+#             */
-/*   Updated: 2024/11/29 13:15:09 by aloubry          ###   ########.fr       */
+/*   Updated: 2024/11/29 13:21:13 by aloubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,54 +25,30 @@ static void	philo_sleep(t_philo *philo)
 
 static void	philo_eat(t_philo *philo)
 {
-<<<<<<< HEAD
-	sem_wait(philo->forks);
-=======
-	sem_wait(*philo->left_fork);
->>>>>>> fb68f8d3794aae15e24533301de9e451f61966f2
+	sem_wait(philo->data->forks_sem);
 	print_action(philo, "has taken a fork");
 	if (philo->data->nb_philo == 1)
 	{
 		ft_usleep(philo->data->time_to_die);
-<<<<<<< HEAD
-		sem_post(philo->forks);
+		sem_post(philo->data->forks_sem);
 		return ;
 	}
-	sem_wait(philo->forks);
-=======
-		sem_post(*philo->left_fork);
-		return ;
-	}
-	sem_post(*philo->right_fork);
->>>>>>> fb68f8d3794aae15e24533301de9e451f61966f2
+	sem_wait(philo->data->forks_sem);
 	print_action(philo, "has taken a fork");
 	print_action(philo, "is eating");
 	sem_wait(philo->last_meal_sem);
 	philo->last_meal = get_time();
 	sem_post(philo->last_meal_sem);
 	ft_usleep(philo->data->time_to_eat);
-<<<<<<< HEAD
-	sem_post(philo->forks);
-	sem_post(philo->forks);
-=======
-	sem_post(*philo->left_fork);
-	sem_post(*philo->right_fork);
->>>>>>> fb68f8d3794aae15e24533301de9e451f61966f2
+	sem_post(philo->data->forks_sem);
+	sem_post(philo->data->forks_sem);
 	sem_wait(philo->eat_count_sem);
 	philo->eat_count++;
 	sem_post(philo->eat_count_sem);
 }
 
-<<<<<<< HEAD
 void	philo_loop(t_philo *philo)
 {
-=======
-void	*philo_loop(void *void_data)
-{
-	t_philo	*philo;
-
-	philo = (t_philo *)void_data;
->>>>>>> fb68f8d3794aae15e24533301de9e451f61966f2
 	if (philo->id % 2 == 0)
 		ft_usleep(1);
 	while (!is_stop(philo->data))
@@ -81,8 +57,4 @@ void	*philo_loop(void *void_data)
 		philo_eat(philo);
 		philo_sleep(philo);
 	}
-<<<<<<< HEAD
-=======
-	return (NULL);
->>>>>>> fb68f8d3794aae15e24533301de9e451f61966f2
 }
--- philo_bonus/setup.c
+++ philo_bonus/setup.c
@@ -6,11 +6,7 @@
 /*   By: aloubry <aloubry@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 13:02:49 by aloubry           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/11/29 13:09:22 by aloubry          ###   ########.fr       */
-=======
-/*   Updated: 2024/11/27 14:02:13 by aloubry          ###   ########.fr       */
->>>>>>> fb68f8d3794aae15e24533301de9e451f61966f2
+/*   Updated: 2024/11/29 13:21:21 by aloubry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +48,6 @@ t_data	parse_and_init_data(int argc, char **argv)
 	else
 		data.nb_philo_eat = -1;
 	data.time_start = get_time();
-<<<<<<< HEAD
 	sem_unlink("/stop_sem");
 	data.stop_sem = sem_open("/stop", O_CREAT, 0);
 	sem_unlink("/print_sem");
@@ -68,48 +63,6 @@ t_philo	*init_philosophers(t_data *data)
 	t_philo	*philosophers;
 	char sem_name[NAME_MAX - 4];
 	char *itoa;
-=======
-	data.stop = 0;
-	sem_unlink("stop_sem");
-	data.stop_sem = sem_open("stop_sem", O_CREAT, 0644, 1);
-	sem_unlink("print_sem");
-	data.print_sem = sem_open("print_sem", O_CREAT, 0644, 1);
-	return (data);
-}
-
-
-
-sem_t	**init_forks(int number_of_forks)
-{
-	sem_t	**forks;
-	char *sem_name;
-	char *itoa;
-	int				i;
-
-	forks = malloc(sizeof(sem_t *) * number_of_forks);
-	if (!forks)
-		return (NULL);
-	i = 0;
-	while (i < number_of_forks)
-	{
-		itoa = ft_itoa(i);
-		sem_name = ft_strjoin("philo_fork_", itoa);
-		free(itoa);
-		sem_unlink(sem_name);
-		forks[i] = sem_open(sem_name, O_CREAT, 0644, 1);
-		free(sem_name);
-		i++;
-	}
-	return (forks);
-}
-
-t_philo	*init_philosophers(t_data *data, sem_t **forks)
-{
-	int		i;
-	t_philo	*philosophers;
-	char *itoa;
-	char *sem_name;
->>>>>>> fb68f8d3794aae15e24533301de9e451f61966f2
 
 	philosophers = malloc(sizeof(t_philo) * data->nb_philo);
 	if (!philosophers)
@@ -118,21 +71,10 @@ t_philo	*init_philosophers(t_data *data, sem_t **forks)
 	while (i < data->nb_philo)
 	{
 		philosophers[i].id = i + 1;
-<<<<<<< HEAD
-=======
-		philosophers[i].left_fork = &forks[i];
-		if (i == data->nb_philo - 1 && data->nb_philo > 1)
-			philosophers[i].right_fork = &forks[0];
-		else if (i < data->nb_philo - 1)
-			philosophers[i].right_fork = &forks[i + 1];
-		else
-			philosophers[i].right_fork = NULL;
->>>>>>> fb68f8d3794aae15e24533301de9e451f61966f2
 		philosophers[i].data = data;
 		philosophers[i].last_meal = data->time_start;
 		philosophers[i].eat_count = 0;
 		itoa = ft_itoa(i);
-<<<<<<< HEAD
 		ft_strlcpy(sem_name, "/last_meal_", NAME_MAX - 4);
 		ft_strlcat(sem_name, itoa, NAME_MAX - 4);
 		sem_unlink(sem_name);
@@ -142,17 +84,6 @@ t_philo	*init_philosophers(t_data *data, sem_t **forks)
 		sem_unlink(sem_name);
 		philosophers[i].eat_count_sem = sem_open(sem_name, O_CREAT, 1);
 		free(itoa);
-=======
-		sem_name = ft_strjoin("last_meal_", itoa);
-		sem_unlink(sem_name);
-		philosophers[i].last_meal_sem = sem_open(sem_name, O_CREAT, 0644, 1);
-		free(sem_name);
-		sem_name = ft_strjoin("eat_count_", itoa);
-		free(itoa);
-		sem_unlink(sem_name);
-		philosophers[i].last_meal_sem = sem_open(sem_name, O_CREAT, 0644, 1);
-		free(sem_name);
->>>>>>> fb68f8d3794aae15e24533301de9e451f61966f2
 		i++;
 	}
 	return (philosophers);
